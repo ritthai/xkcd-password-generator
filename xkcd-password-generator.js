f81e554578c1bad71525b0c159d70e3572358518
Ritchie Thai <RitchieThai@RitchieThai.com>
2014-07-17 04:18:17 -0400

    Autoindented xkcd-password-generator.js using LightTable

--- xkcd-password-generator.js
+++ xkcd-password-generator.js
@@ -6,34 +6,34 @@ See the file license.txt for copying permission.
 
 var makeXkcdPasswordGenerator = function () {
 
-passwordWordCount = 4;
+  passwordWordCount = 4;
 
-var bitCountInUint32 = 32;
+  var bitCountInUint32 = 32;
 
-var getCryptographicallySecureRandomNumber = function (max) {
+  var getCryptographicallySecureRandomNumber = function (max) {
     var bitsOfEntropyCount = Math.log(max + 1) / Math.LN2;
     var bitsToRemoveCount = bitCountInUint32 - bitsOfEntropyCount;
     while (true) {
-        var randomValues = new Uint32Array(1);
-        window.crypto.getRandomValues(randomValues);
-        var randomValue = Math.abs(randomValues[0] >> bitsToRemoveCount);
-        if (randomValue <= max) {
-            return randomValue;
-        }
+      var randomValues = new Uint32Array(1);
+      window.crypto.getRandomValues(randomValues);
+      var randomValue = Math.abs(randomValues[0] >> bitsToRemoveCount);
+      if (randomValue <= max) {
+        return randomValue;
+      }
     }
-};
+  };
 
-var generatePassword = function () {
+  var generatePassword = function () {
     var password = '';
     for (var i = 0; i < passwordWordCount; i++) {
-        if (password) {
-          password += ' ';
-        }
-        password += words[getCryptographicallySecureRandomNumber(words.length)];
+      if (password) {
+        password += ' ';
+      }
+      password += words[getCryptographicallySecureRandomNumber(words.length)];
     }
     return password;
-}
+  }
 
-return { generatePassword: generatePassword };
+  return { generatePassword: generatePassword };
 
 };
